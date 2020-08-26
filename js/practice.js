9e304e496c6e00b3fc738d7ecf2b6259eabc31c3
Tetiana Bronitska <tanya.nest87@gmail.com>
2020-08-26 14:15:01 +0300

    Task 1-2

---
+++ js/practice.js
@@ -0,0 +1,46 @@
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: true },
+//   { name: 'Ajax', online: false },
+// ];
+
+//console.table(friends);
+
+// const getAllNames = function (allFriends) {
+//   const names = [];
+//   for (const friend of allFriends) {
+//     console.log(friend.name);
+//     names.push(friend.name);
+//   }
+
+//   return names;
+// };
+
+// console.log(getAllNames(friends));
+
+// const getOnLineFriends = function (allFriends) {
+//   const friendsOnLine = [];
+
+//   for (const friend of allFriends) {
+//     if (friend.online) {
+//       friendsOnLine.push(friend);
+//     }
+//   }
+//   return friendsOnLine;
+// };
+
+// console.log(getOnLineFriends(friends));
+
+// const playlist = {
+//   name: 'Мой супер плейлист',
+//   rating: 5,
+//   tracks: ['трек-1', 'трек-2', 'трек-3'],
+//   trackCount: 3,
+// };
+
+// playlist.qwe = 5;
+
+// playlist.rating = 10;
+
+// console.log(playlist);
